7fce35cccfa978fe53f891fd566471a3be5bd1e7
Krugou <59641229+Krugou@users.noreply.github.com>
2024-08-30 21:39:50 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Home = () => {
   const [messages, setMessages] = useState<{text: string; isUser: boolean}[]>(
@@ -16,56 +16,55 @@ const Home = () => {
   const replies = [
     {trigger: 'hello', response: 'Hi there!'},
     {trigger: 'how are you', response: 'I am fine, thank you!'},
-    {trigger: 'bye', response: 'Goodbye!'},
-    {trigger: 'oletko sinä ihminen?', response: 'En, olen botti.'},
-    {
-      trigger: 'mikä on nimesi?',
-      response: 'Nimeni on My Business Finland botti',
-    },
-    {
-      trigger: 'mikä on tehtäväsi?',
-      response:
-        'Autan sinua löytämään tietoa yrityksen perustamisesta Suomessa.',
-    },
-    {
-      trigger: 'mikä on yrityksen perustaminen?',
-      response:
-        'Yrityksen perustaminen on prosessi, jossa perustetaan uusi yritys.',
-    },
-    {
-      trigger: 'miten perustan yrityksen?',
-      response: 'Sinun täytyy rekisteröityä yrittäjäksi ja perustaa yritys.',
-    },
-    {
-      trigger: 'mikä on y-tunnus?',
-      response:
-        'Y-tunnus on yrityksen tunnus, jota käytetään verotuksessa ja viranomaisasioinnissa.',
-    },
-    {
-      trigger: 'mikä on prh?',
-      response:
-        'PRH on Patentti- ja rekisterihallitus, joka vastaa yritysten rekisteröinnistä.',
-    },
-    {
-      trigger: 'mitä tarkoittaa osakeyhtiö?',
-      response:
-        'Osakeyhtiö on yritysmuoto, jossa omistajat vastaavat rajoitetusti yrityksen veloista.',
-    },
-    {
-      trigger: 'miten perustan osakeyhtiön?',
-      response:
-        'Sinun täytyy rekisteröityä yrittäjäksi ja perustaa osakeyhtiö.',
-    },
-    {
-      trigger: 'mikä on toiminimi?',
-      response:
-        'Toiminimi on yritysmuoto, jossa yrityksen ja omistajan taloudellinen vastuu on yksi ja sama.',
-    },
-    {
-      trigger: 'miten perustan toiminimen?',
-      response: 'Sinun täytyy rekisteröityä yrittäjäksi ja perustaa toiminimi.',
-    },
-    // Add more preset replies as needed
+    // {trigger: 'bye', response: 'Goodbye!'},
+    // {trigger: 'oletko sinä ihminen?', response: 'En, olen botti.'},
+    // {
+    //   trigger: 'mikä on nimesi?',
+    //   response: 'Nimeni on My Business Finland botti',
+    // },
+    // {
+    //   trigger: 'mikä on tehtäväsi?',
+    //   response:
+    //     'Autan sinua löytämään tietoa yrityksen perustamisesta Suomessa.',
+    // },
+    // {
+    //   trigger: 'mikä on yrityksen perustaminen?',
+    //   response:
+    //     'Yrityksen perustaminen on prosessi, jossa perustetaan uusi yritys.',
+    // },
+    // {
+    //   trigger: 'miten perustan yrityksen?',
+    //   response: 'Sinun täytyy rekisteröityä yrittäjäksi ja perustaa yritys.',
+    // },
+    // {
+    //   trigger: 'mikä on y-tunnus?',
+    //   response:
+    //     'Y-tunnus on yrityksen tunnus, jota käytetään verotuksessa ja viranomaisasioinnissa.',
+    // },
+    // {
+    //   trigger: 'mikä on prh?',
+    //   response:
+    //     'PRH on Patentti- ja rekisterihallitus, joka vastaa yritysten rekisteröinnistä.',
+    // },
+    // {
+    //   trigger: 'mitä tarkoittaa osakeyhtiö?',
+    //   response:
+    //     'Osakeyhtiö on yritysmuoto, jossa omistajat vastaavat rajoitetusti yrityksen veloista.',
+    // },
+    // {
+    //   trigger: 'miten perustan osakeyhtiön?',
+    //   response:
+    //     'Sinun täytyy rekisteröityä yrittäjäksi ja perustaa osakeyhtiö.',
+    // },
+    // {
+    //   trigger: 'mikä on toiminimi?',
+    //   response:
+    //     'Toiminimi on yritysmuoto, jossa yrityksen ja omistajan taloudellinen vastuu on yksi ja sama.',
+    // },
+    // {
+    //   trigger: 'miten perustan toiminimen?',
+    //   response: 'Sinun täytyy rekisteröityä yrittäjäksi ja perustaa toiminimi.',
+    // },
   ];
 
   useEffect(() => {
@@ -106,7 +105,7 @@ const Home = () => {
     }
   };
 
-  const handleReplyClick = async (reply:string) => {
+  const handleReplyClick = async (reply: string) => {
     await setInputValue(reply);
     handleSendMessage();
   };
