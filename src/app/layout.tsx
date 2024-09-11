d832653399a14ef63a79a3213c91009999b95350
Krugou <59641229+Krugou@users.noreply.github.com>
2024-09-11 18:07:56 +0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,26 @@ import React from 'react';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Onboarding testing',
-  description: 'Onboarding testing description',
+  title: 'Business Finland Onboarding Portal',
+  description:
+    'Welcome to the Business Finland Onboarding Portal. This is where you complete the onboarding process.',
+  openGraph: {
+    images: [
+      {
+        url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Business_Finland.jpg/220px-Business_Finland.jpg',
+        width: 220,
+        height: 124,
+        alt: 'Business Finland',
+      },
+    ],
+  },
 };
 console.log('Website rendered in ' + process.env.NODE_ENV);
 const RootLayout = ({children}: {children: React.ReactNode}) => (
   <html lang='en'>
+    <head>
+      <link rel='icon' href='/icons/favicon-32x32.webp' />
+    </head>
     <body className='font-mainFont'>{children}</body>
   </html>
 );
