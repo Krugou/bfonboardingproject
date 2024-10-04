b79665b98820f242d7d292ef0c2c7d660bb2a550
Krugou <59641229+Krugou@users.noreply.github.com>
2024-10-04 09:43:05 +0300

    Refactor AnsweredQuestionsModal component to improve UI and display answered questions with additional information

--- src/components/AnsweredQuestionsModal.tsx
+++ src/components/AnsweredQuestionsModal.tsx
@@ -26,40 +26,86 @@ const AnsweredQuestionsModal: React.FC<AnsweredQuestionsModalProps> = ({
   );
 
   return (
-    <div className='fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center'>
-      <div className='bg-white rounded-lg p-6 w-full xl:w-1/3'>
+    <div className='fixed inset-0 bg-gray-600  bg-opacity-50 flex justify-center items-center'>
+      <div className='bg-white rounded-lg p-6 w-full xl:w-2/3'>
         <h2 className='text-xl font-bold mb-4'>
           {!language ? 'Answered Questions' : 'Vastatut kysymykset'}
         </h2>
         <div className='mb-4'>
           {answeredQuestions.length > 0 ? (
             <ul className='list-disc list-inside'>
-              {answeredQuestions.map((q, index) => (
-                <li
-                  key={q.id}
-                  className='flex flex-col sm:flex-row justify-between items-start sm:items-center mb-4'>
-                  <span className='break-words w-1/3 '>
-                    {q.question[language]}
-                  </span>
-                  <span className='ml-4 text-gray-600 border border-black p-4 rounded-xl'>
-                    {userInfo.questionAnswers[q.id]}
-                  </span>
-                  <button
-                    className='ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded'
-                    onClick={() => setCurrentStep(index + 1)}>
-                    {!language
-                      ? 'Go Back to this Question'
-                      : 'Palaa tähän kysymykseen'}
-                  </button>
-                </li>
-              ))}
+              {answeredQuestions?.map((q, index) => {
+                if (q.id === 'k1.2') {
+                  return (
+                    <li
+                      key={q.id}
+                      className='flex flex-col sm:flex-row justify-between items-start sm:items-center gap-2 mb-4'>
+                      <span className=''>{q.id}</span>
+                      <span className=' '>{q.question[language]}</span>
+                      <div className='flex flex-col justify-start border border-black rounded-xl p-4 w-full'>
+                        {userInfo?.questionAnswers[q.id]?.industry && (
+                          <span className='text-gray-600 mb-2'>
+                            {language === 'fi' ? 'Toimiala' : 'Industry'}:{' '}
+                            {userInfo?.questionAnswers[q.id]?.industry}
+                          </span>
+                        )}
+                        {userInfo?.questionAnswers[q.id]?.address && (
+                          <span className='text-gray-600 mb-2'>
+                            {language === 'fi' ? 'Osoite' : 'Address'}:{' '}
+                            {userInfo?.questionAnswers[q.id]?.address}
+                          </span>
+                        )}
+                        {userInfo?.questionAnswers[q.id]?.numberOfEmployees && (
+                          <span className='text-gray-600 mb-2'>
+                            {language === 'fi'
+                              ? 'Henkilömäärä'
+                              : 'Number of Employees'}
+                            :{' '}
+                            {userInfo?.questionAnswers[q.id]?.numberOfEmployees}
+                          </span>
+                        )}
+                        {userInfo?.questionAnswers[q.id]?.wwwAddress && (
+                          <span className='text-gray-600'>
+                            {language === 'fi' ? 'www' : 'www'}:{' '}
+                            {userInfo?.questionAnswers[q.id]?.wwwAddress}
+                          </span>
+                        )}
+                      </div>
+                      <button
+                        className='ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1  px-2 rounded'
+                        onClick={() => setCurrentStep(index + 1)}>
+                        {language === 'fi'
+                          ? 'Palaa tähän kysymykseen'
+                          : 'Go Back to this Question'}
+                      </button>
+                    </li>
+                  );
+                }
+                return (
+                  <li
+                    key={q.id}
+                    className='flex flex-col sm:flex-row justify-between items-start sm:items-center gap-2 mb-4'>
+                    <span className=' '>{q.id}</span>
+                    <span className=' '>{q.question[language]}</span>
+                    <span className='ml-4 text-gray-600 border border-black p-4 rounded-xl'>
+                      {userInfo?.questionAnswers[q.id]}
+                    </span>
+                    <button
+                      className='ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded'
+                      onClick={() => setCurrentStep(index + 1)}>
+                      {language === 'fi'
+                        ? 'Palaa tähän kysymykseen'
+                        : 'Go Back to this Question'}
+                    </button>
+                  </li>
+                );
+              })}
             </ul>
           ) : (
             <p>
-              {' '}
               {language === 'fi'
                 ? 'Ei vastattuja kysymyksiä'
-                : 'No answered questions'}{' '}
+                : 'No answered questions'}
             </p>
           )}
         </div>
