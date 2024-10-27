e3ac11c7585fedf4082d134c3eac97f9292c3da8
Krugou <59641229+Krugou@users.noreply.github.com>
2024-10-27 16:42:13 +0200

    Refactor layout and add Google Analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
+import GoogleStartup from '@/components/GoogleStartup';
 import { UserProvider } from '@/context/UserContext';
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Favicon from './favicon.webp';
 import './globals.css';
-import { initializeAnalytics } from '@/utils/analytics';
 
 const finlandica = localFont({
   src: './Finlandica/Finlandica-VariableFont_wght.ttf',
@@ -14,7 +14,7 @@ const finlandica = localFont({
 export const metadata: Metadata = {
   title: 'Business Finland Onboarding Portal',
   description:
-    'Welcome to the Business Finland Onboarding Portal. This is where you complete the onboarding process.',
+    'Welcome to the Business Finland Onboarding Portal. This is where you complete the onboarding process. Get started by logging in.',
   openGraph: {
     images: [
       {
@@ -29,14 +29,13 @@ export const metadata: Metadata = {
 };
 // console.log('Website rendered in ' + process.env.NODE_ENV);
 const RootLayout = ({children}: {children: React.ReactNode}) => {
-  useEffect(() => {
-    initializeAnalytics();
-  }, []);
+
 
   return (
     <html lang='en'>
       <body className={finlandica.className}>
         <UserProvider>
+          <GoogleStartup />
           <ToastContainer />
           {children}
         </UserProvider>
---
+++ src/components/GoogleStartup.tsx
@@ -0,0 +1,17 @@
+import { initializeAnalytics } from '@/utils/analytics';
+import React, { useEffect } from 'react';
+const GoogleStartup: React.FC = () => {
+  useEffect(() => {
+    initializeAnalytics();
+  }, []);
+
+
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default GoogleStartup;
