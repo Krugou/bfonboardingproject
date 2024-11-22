3ad02a41de935eac9c2d6244f52e1a779ac9ea73
Krugou <59641229+Krugou@users.noreply.github.com>
2024-11-22 14:03:13 +0200

    Update button styles to enable cursor interaction; adjust HistoryButton logic for better state handling

--- src/components/MainContent/UpperPanel/HistoryButton.tsx
+++ src/components/MainContent/UpperPanel/HistoryButton.tsx
@@ -1,14 +1,18 @@
 import {useUserContext} from '@/context/UserContext';
 import React from 'react';
+
 interface HistoryButtonProps {
   onClick: () => void;
 }
 
 const HistoryButton: React.FC<HistoryButtonProps> = ({onClick}) => {
-  const {language, currentStep} = useUserContext();
-  const isDisabled = currentStep === 1;
+  const {language, userInfo} = useUserContext();
+  const isDisabled =
+    !userInfo?.questionAnswers ||
+    Object.keys(userInfo.questionAnswers).length === 0;
+
   return (
-    <div className='flex justify-end  h-full  dark:bg-gray-200 dark:text-white'>
+    <div className='flex justify-end h-full dark:bg-gray-200 dark:text-white'>
       <button
         className={`primary-button ${
           isDisabled ? 'opacity-50 cursor-not-allowed' : ''
