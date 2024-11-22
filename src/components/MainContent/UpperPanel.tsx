6eceec8eae094c3577fe97ba300a75fcf2cfcd3e
Krugou <59641229+Krugou@users.noreply.github.com>
2024-11-22 13:45:57 +0200

    Update button styles to disable cursor interaction; refactor layout in UpperPanel and HistoryButton components

--- src/components/MainContent/UpperPanel.tsx
+++ src/components/MainContent/UpperPanel.tsx
@@ -1,26 +1,17 @@
-import { useUserContext } from '@/context/UserContext';
+import {useUserContext} from '@/context/UserContext';
 import React from 'react';
 import BFStepper from './UpperPanel/BFStepper';
 import HistoryButton from './UpperPanel/HistoryButton';
 import ListeningModeToggle from './UpperPanel/ListeningModeToggle';
 interface UpperPanelProps {
-
-
   handleOpenModal: () => void;
 }
 
-const UpperPanel: React.FC<UpperPanelProps> = ({
-
-
-  handleOpenModal,
-}) => {
+const UpperPanel: React.FC<UpperPanelProps> = ({handleOpenModal}) => {
   return (
-    <div className='flex flex-col sm:flex-row justify-between mx-4 sm:mx-6 md:mx-8 lg:mx-10 mt-4 sm:mt-6 md:mt-8 lg:mt-10 dark:bg-gray-800 dark:text-white'>
-      <ListeningModeToggle
-
-
-      />
-      <BFStepper  />
+    <div className='flex  justify-between items-center  '>
+      <ListeningModeToggle />
+      <BFStepper />
       <HistoryButton
         onClick={handleOpenModal}
         aria-label='Open history modal'
--- src/components/MainContent/UpperPanel/ListeningModeToggle.tsx
+++ src/components/MainContent/UpperPanel/ListeningModeToggle.tsx
@@ -1,16 +1,12 @@
-import { useUserContext } from '@/context/UserContext';
+import {useUserContext} from '@/context/UserContext';
 import MicIcon from '@mui/icons-material/Mic';
 import MicOffIcon from '@mui/icons-material/MicOff';
 import React from 'react';
 
-interface ListeningModeToggleProps {
+interface ListeningModeToggleProps {}
 
-}
-
-const ListeningModeToggle: React.FC<ListeningModeToggleProps> = ({
-
-}) => {
-  const {language ,listeningMode,setListeningMode} = useUserContext();
+const ListeningModeToggle: React.FC<ListeningModeToggleProps> = ({}) => {
+  const {language, listeningMode, setListeningMode} = useUserContext();
 
   const titleText = listeningMode
     ? language === 'fi'
@@ -22,7 +18,7 @@ const ListeningModeToggle: React.FC<ListeningModeToggleProps> = ({
 
   return (
     <div
-      className={`flex gap-1 justify-center items-center w-1/3 dark:bg-gray-800 dark:text-white ${
+      className={`flex gap-1 justify-center items-center   ${
         listeningMode ? '' : ''
       }`}
       title={titleText}>
@@ -36,7 +32,9 @@ const ListeningModeToggle: React.FC<ListeningModeToggleProps> = ({
           onChange={() => setListeningMode(!listeningMode)}
           role='switch'
           aria-checked={listeningMode}
-          aria-label={listeningMode ? 'Turn off listening mode' : 'Turn on listening mode'}
+          aria-label={
+            listeningMode ? 'Turn off listening mode' : 'Turn on listening mode'
+          }
         />
         <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
         <span className='text-sm font-medium text-gray-900 ms-1 dark:text-gray-300'></span>
